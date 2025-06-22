c026e7e9607f06619e19bc0e2450156597bac505
Kanishka <kanishkap2308@gmail.com>
2025-06-22 17:16:34 +0530

    comment

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -44,16 +44,16 @@ const Header = () => {
         />
       </div>
 
-//navigation bar
+{/* //navigation bar */}
       <nav className="w-full bg-sky-600 text-white px-4 py-2 md:px-0 md:py-0">
-// Mobile Menu Toggle
+{/* // Mobile Menu Toggle */}
         <div className="md:hidden flex justify-between items-center">
           <span className="font-medium">Menu</span>
           <button onClick={() => setMenuOpen(!menuOpen)}>
             {menuOpen ? <X size={24} /> : <Menu size={24} />}
           </button>
         </div>
-// Navbar Items
+{/* // Navbar Items */}
         <ul
           className={`flex flex-col md:flex-row md:justify-center gap-4 md:gap-6 text-sm sm:text-base font-medium py-2 md:py-3 transition-all duration-300 ease-in-out ${
             menuOpen ? "block" : "hidden md:flex"
